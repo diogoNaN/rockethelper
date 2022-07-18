5538f9856f7b80f711307ad8da52dc03ff1322df
DiogoNaN <diogonan@protonmail.ch>
2022-07-18 20:11:52 -0300

    change Loading size

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { Center, Spinner } from "native-base";
 export default function Loading() {
   return (
     <Center flex={1} bg="gray.700">
-      <Spinner color="secondary.700" />
+      <Spinner color="secondary.700" size={"lg"} />
     </Center>
   );
 }
