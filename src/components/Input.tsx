0bef824522d31d3866ffcc04c4f773a1c34fc023
DiogoNaN <diogonan@protonmail.ch>
2022-07-18 20:05:21 -0300

    add Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,22 @@
+import { IInputProps, Input as NativeBaseInput } from "native-base";
+
+export default function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput
+      bg="gray.700"
+      h={14}
+      size="md"
+      borderWidth={1}
+      borderColor="gray.700"
+      fontSize="md"
+      fontFamily={"body"}
+      color="white"
+      placeholderTextColor={"gray.300"}
+      _focus={{
+        borderColor: "green.500",
+        bg: "gray.700",
+      }}
+      {...rest}
+    />
+  );
+}
