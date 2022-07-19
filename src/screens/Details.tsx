2542ad56918329b92ef11e8af33878ef379dacf7
DiogoNaN <diogonan@protonmail.ch>
2022-07-19 20:09:16 -0300

    add Details screen

---
+++ src/screens/Details.tsx
@@ -0,0 +1,11 @@
+import { VStack } from "native-base";
+
+import Header from "../components/Header";
+
+export default function Details() {
+  return (
+    <VStack flex={1} p={6} bg="gray.700">
+      <Header title="Solicitação" />
+    </VStack>
+  );
+}
